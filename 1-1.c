742d84483eca42567afcba15aaeb74441138be3a
madhav_18 <159808252+18-Madhav-9@users.noreply.github.com>
2024-07-02 11:07:03 +0530

    Add files via upload

---
+++ 1-1.c
@@ -0,0 +1,13 @@
+/*
+* Exercise 1-1 Hello World Program but leaving out parts of the original program
+*/
+
+//leaving out the header
+//#include <stdio.h>
+
+main()
+{
+    printf("hello, ");
+    printf("world!");
+    printf("\n");
+}
