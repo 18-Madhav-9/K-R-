27b90be58217ec88b1cd316878cd6fbbf1254d93
madhav_18 <159808252+18-Madhav-9@users.noreply.github.com>
2024-07-07 12:42:05 +0530

    Add files via upload

---
+++ 1-15.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void fahr_celsius() ;
+
+int main()
+{
+    fahr_celsius() ;
+    return 0 ;
+}
+
+void fahr_celsius()
+{
+    float fahr , celsius ;
+    printf("Fahrenheit To Celsius \n");
+    for (fahr = 0 ; fahr <= 200 ; fahr = fahr + 20)
+    {
+        celsius = (5.0/9.0) * (fahr - 32);
+        printf("%5.0f  -> %5.2f \n",fahr,celsius);
+    }
+
+}
